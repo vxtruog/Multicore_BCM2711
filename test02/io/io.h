a1400940bf6abb713d57861278058d49b62ada4c
Vu Xuan Truong <truongvx.2001@gmail.com>
2025-02-16 16:03:12 +0700

    Add files via upload

---
+++ test02/io/io.h
@@ -0,0 +1,7 @@
+#ifndef __IO_H__
+#define __IO_H__
+
+void uart_init();
+void uart_writeText(char *buffer);
+
+#endif
---
+++ test02/kernel.c
@@ -0,0 +1,7 @@
+#include "io.h"
+
+void main(){
+    uart_init();
+    uart_writeText("Hello world!\n");
+    while(1);
+}
